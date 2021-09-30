3b73bad12388e171eb335ab3549aa83081c98c68
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-30 11:30:47 +0000

    [start3]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,7 +3,8 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @book_comment = BookComment.new(book_comment_params)
-    @book_comment.book_id = current_user.id
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
     if @book_comment.save
     redirect_to book_path(@book.id)
     else
